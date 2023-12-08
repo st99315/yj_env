bd9fb8af4deadeca0c586def10d30dad241fe025
youjun <eric_huang@solomon-3d.com>
2023-12-08 16:56:03 +0800

    Add LazyVim Setup

---
+++ nvim/config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- latte, frappe, macchiato, mocha
+      flavour = "macchiato",
+      -- doesn't work
+      show_end_of_buffer = true,
+    },
+  },
+}
